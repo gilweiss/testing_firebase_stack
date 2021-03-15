62a30999704f09d79dfe0bb7b8c031d22da483aa
gilweiss <gilweiss90@gmail.com>
2021-03-15 11:15:08 +0200

    first upload

---
+++ src/header.js
@@ -0,0 +1,13 @@
+export const Header = (props) => {
+
+    return (
+        <span>
+            <table className="transparentTable">
+                    <thead><tr className="header"><td className="framMe">Log In</td><td className="tdAlignRight"><b>My Fabricated Store</b></td>
+                    </tr></thead><tbody>
+                    </tbody></table>
+        </span>
+    );
+}
+
+
